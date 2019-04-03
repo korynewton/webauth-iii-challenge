360ce8be4d86b00f46549ab9b3bca6789ba1f669
korynewton <korynewtondev@gmail.com>
2019-04-03 15:06:39 -0700

    GET request to /api/users responds with array of users only if token has been receieved and verified

--- users/usersRoutes.js
+++ users/usersRoutes.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.status(200).json({ message: "users routes connected"})
+const secret = require('../secrets');
+const Users = require('../models/models');
+const jwt = require('jsonwebtoken');
+
+router.get('/', async (req, res) => {
+    const token = req.headers.authorization;
+    if (token) {
+        try {
+            jwt.verify(token, secret.jwtSecret)
+            const users = await Users.find();
+            res.status(200).json(users)
+        } catch(error) {
+            res.status(500).json(error)
+        }
+    } else {
+        res.status(500).json({ message:"You shall not pass" })
+    }
 })
 
 module.exports = router;
