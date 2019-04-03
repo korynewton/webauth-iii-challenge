92fb942b55db28a797a7a36e6b6b33497a462075
korynewton <korynewtondev@gmail.com>
2019-04-03 14:14:16 -0700

    login post working, returns jwt to user upon successfull login

--- auth/authRoutes.js
+++ auth/authRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const Auth = require('../models/models')
+const jwt = require('jsonwebtoken');
+const secret = require('../secrets').jwtSecret
+
 
 
 const router = express.Router();
@@ -15,13 +18,38 @@ router.post('/register', async (req, res) => {
             res.status(200).json(register)
         } else {
             res.status(400).json({message:"user already added"})
-            
         }
     } catch(error) {
         res.status(500).json(error)
-
     }
 })
+
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const user = await Auth.findBy({ username }).first();
+        if (user && bcrypt.compareSync(password, user.password)) {
+            const token = generateToken(user);
+            res.status(200).json({ message:`welcome ${user.username}`, token })
+        } else {
+            res.status(400).json({ message: "incorrect user or pass" })
+        }
+
+    } catch(error) {
+        res.status(500).json({ message:"you shall not pass", error })
+    }
+})
+
+function generateToken(user) {
+    const payload = {
+      subject : user.id,
+      username : user.username,
+    }
+    const options = {
+      expiresIn: "1d"
+    }  
+    return jwt.sign(payload, secret, options);
+  }
 
 module.exports = router;
 
